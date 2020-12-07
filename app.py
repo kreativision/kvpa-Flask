2a7f9f3932224b016061613a969ca7f8526f31e4
Amittras <pal.amittras@gmail.com>
2020-12-07 21:58:20 +0530

    added dependencies for mysql; updated sign-up page with password field.

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, session, logging, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+# will use later!
+# from passlib.hash import sha256_crypt 
 
 app = Flask(__name__)
+engine = create_engine("mysql+pymysql://root:dbRootKVPA@localhost/kv_schema")
+db=scoped_session(sessionmaker(bind=engine))
 
 
 @app.route('/')
@@ -10,7 +17,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/sign-up')
+@app.route('/sign-up', )
 def signUp():
     return render_template('sign-up.html')
 
