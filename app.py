4ae2af47d1b46dd506f1236c6d9c27843c5a4cd0
Amittras <pal.amittras@gmail.com>
2020-12-07 22:00:02 +0530

    update gitignore

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, url_for, request, session, logging, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import scoped_session, sessionmaker
 
 # will use later!
 # from passlib.hash import sha256_crypt 
 
 app = Flask(__name__)
-engine = create_engine("mysql+pymysql://root:dbRootKVPA@localhost/kv_schema")
-db=scoped_session(sessionmaker(bind=engine))
+# engine = create_engine("mysql+pymysql://root:dbRootKVPA@localhost/kv_schema")
+# db=scoped_session(sessionmaker(bind=engine))
 
 
 @app.route('/')
@@ -17,7 +17,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/sign-up', )
+@app.route('/sign-up')
 def signUp():
     return render_template('sign-up.html')
 
