5ed0270ac63f57fa3de64f3b485257bb3fcdf16b
Amittras <pal.amittras@gmail.com>
2020-12-09 08:49:03 +0530

    Added DB Config file for keeping db credentials;

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template, url_for, request, session, logging, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
+from dbConfig import db
 # from sqlalchemy import create_engine
 # from sqlalchemy.orm import scoped_session, sessionmaker
 
-# will use later!
-# from passlib.hash import sha256_crypt 
 
 app = Flask(__name__)
-# engine = create_engine("mysql+pymysql://root:dbRootKVPA@localhost/kv_schema")
-# db=scoped_session(sessionmaker(bind=engine))
+db_config = f'{db.category}://{db.user}:{db.pwd}@{db.url}:{db.port}/{db.name}'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_config
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class member(db.Model):
+    ph_no = db.Column(db.String(40), primary_key=True)
+    first_name = db.Column(db.String(40), unique = True)
+    last_name = db.Column(db.String(40))
+    email = db.Column(db.String(40), unique = True)
+    password = db.Column(db.String(20))
+
+    def __init__(self, ph_no, password):
+        self.ph_no = ph_no
+        self.password = password
+
 
 
 @app.route('/')
---
+++ dbConfig.py
@@ -0,0 +1,7 @@
+class db: 
+    category = 'mysql'
+    user = 'root'
+    pwd = 'dbRootKVPA'
+    url = 'localhost'
+    port = '3306'
+    name = 'kv_schema'
