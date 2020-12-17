affdac26fd9e8200e6f749f5a7b21594846a7957
Amittras <pal.amittras@gmail.com>
2020-12-17 16:46:21 +0530

    added _global.scss to manage common styles for all pages; Feature: Username input -> automatic title case; Bug Fix: password toggle checkbox doesn't work with keyboard

--- static/js/form.js
+++ static/js/form.js
@@ -1,31 +1,49 @@
+// registers form inputs on page load.
 var formFields;
+// locks the label on top of the field if text is entered into it. 
+// otherwise restores the label in it's default position.
 document.addEventListener("DOMContentLoaded", () => {
-    formFields = document.querySelectorAll('.form-group .form-control');
-    formFields.forEach((e) => {
-        if(e.value != '') {
-            e.parentNode.querySelector('label').classList.add('active');
-        } else {
-            e.parentNode.querySelector('label').classList.remove('active');
-        }
-        e.onblur = function() {
-            if(e.value != '') {
-                e.parentNode.querySelector('label').classList.add('active');
-            } else {
-                e.parentNode.querySelector('label').classList.remove('active');
-            }
-        }
-    });
-});
-var passwords;
-function visibility() {
-    if(!passwords) {
-        passwords = document.querySelectorAll('input[type="password"]');
+  formFields = document.querySelectorAll('.form-group .form-control');
+  formFields.forEach((e) => {
+    if (e.value != '') {
+      e.parentNode.querySelector('label').classList.add('active');
+    } else {
+      e.parentNode.querySelector('label').classList.remove('active');
     }
-    passwords.forEach(field => {
-        if(field.type == 'text') {
-            field.type = 'password';
-        } else {
-            field.type = 'text';
-        }
-    });
+    e.onblur = function () {
+      if (e.value != '') {
+        e.parentNode.querySelector('label').classList.add('active');
+      } else {
+        e.parentNode.querySelector('label').classList.remove('active');
+      }
+    }
+  });
+});
+
+// registers password fields on page-load. 
+var passwords;
+// toggles the visibility of password on checking the checkbox "Show Password"
+function togglePassword() {
+  if (!passwords) {
+    passwords = document.querySelectorAll('input[type="password"]');
+  }
+  passwords.forEach(field => {
+    if (field.type == 'text') {
+      field.type = 'password';
+    } else {
+      field.type = 'text';
+    }
+  });
+}
+
+// registers the username field on register page.
+// function to automatically capitalize first letter of every word in the name field.
+var username = document.getElementById('username');
+if (username) {
+  username.onkeyup = function () {
+    value = username.value;
+    username.value = value.split(" ").map((s) => {
+      return s.charAt(0).toUpperCase() + s.substring(1).toLowerCase();
+    }).join(" ");
+  };
 }
